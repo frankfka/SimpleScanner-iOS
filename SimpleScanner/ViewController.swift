51f3fa642c8cd8b0a1ad57d16282fe4addbc1994
Frank Jia <jiafrank98@gmail.com>
2019-05-26 13:31:24 -0700

    Initial Commit

---
+++ SimpleScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleScanner
+//
+//  Created by Frank Jia on 2019-05-26.
+//  Copyright © 2019 Frank Jia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
